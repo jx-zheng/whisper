032349fb76f1aabaca5def3f567fc095ce287ff2
Kevin Zheng <jxzheng@uwaterloo.ca>
2023-05-12 01:33:29 -0400

    set up maven project

---
+++ src/test/java/com/jxzheng/whisper/AppTest.java
@@ -0,0 +1,20 @@
+package com.jxzheng.whisper;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
