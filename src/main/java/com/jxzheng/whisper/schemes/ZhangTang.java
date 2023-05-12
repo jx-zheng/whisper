bef6dd9dba664e780fa09c83f370d1961ac3885c
Kevin Zheng <jxzheng@uwaterloo.ca>
2023-05-12 03:12:30 -0400

    add stubs for two schemes

---
+++ src/main/java/com/jxzheng/whisper/schemes/ZhangTang.java
@@ -0,0 +1,19 @@
+package com.jxzheng.whisper.schemes;
+
+import java.awt.image.BufferedImage;
+
+public class ZhangTang extends AbstractScheme {
+
+    @Override
+    public BufferedImage embedMessage(String key, String message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'embedMessage'");
+    }
+
+    @Override
+    public String extractMessage(String key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'extractMessage'");
+    }
+    
+}
